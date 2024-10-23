c5b0c597fae303330401fdeae0f954a0c998bb67
parularora1703 <parularora1703@gmail.con>
2024-10-23 15:36:30 +0530

    hbs+partials

--- Server/server.js
+++ Server/server.js
@@ -3,6 +3,8 @@ const connectDb = require("./config/dbConnection.js");
 const errorHandler = require("./middlewares/errorHandler");
 const cors = require("cors");
 const dotenv = require("dotenv").config();
+const hbs = require("hbs");
+const path = require("path");
 
 connectDb();
 const app = express();
@@ -16,6 +18,19 @@ app.set('view engine','hbs');
 app.get('/', (req, res) => {
     res.send("Working");
 })
+
+app.get('/home', (req, res) => {
+    res.render("home",{username: "parul"});
+})
+
+app.get("/allusers",(req,res) => {
+    res.render("users", {
+        users:[{id:1,username:"Parul",age:20},
+            {id:2,username:"Jiya",age:15}
+        ]})
+})
+
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
 app.listen(port, () => {
     console.log(`Server is running on port http://localhost:${port}`);
